c2d615dc86bfd2bfa0682ec04d42aa4b3e41f560
Amrit Rai <amritrai5757@gmail.com>
2024-01-03 17:57:47 +0530

    Update and rename Subset Permutation Unbounded & 01.cpp to Subset Permutation Unbounded & 01.py

--- Recursion and Dp/Subset Permutation Unbounded & 01.py
+++ Recursion and Dp/Subset Permutation Unbounded & 01.py
@@ -1,22 +1,28 @@
 // Like all other dp questions all permutation except printing can be solved using adding dp
 //     eg: finding no of permutations having sum equal to target (1d dp since in void dfs(..,int sum) only 1 variable sum)
 
-void dfs(vector<int>& num, vi &vis, vi &temp, vector<vi>&ans){             //😍😍😍we dont pass index c in permutation, we call dfs for i=0 to nums.size()                      
-        if(temp.size()==num.size()){                                        
-            ans.push_back(temp);
-        }
+class Solution:
+    def dfs (self, nums, temp, ans,vis):                              #😍😍😍we dont pass index c in permutation, we call dfs for i=0 to nums.size() 
+        if(len(temp)==len(nums)):
+            ans.append(temp.copy())
+            return
         
-        for(int i = 0; i < num.size(); i++){
-            if(vis[i] == 0){                                             //Remove for unbounded
-                if (i > 0 && num[i] == num[i-1] && vis[i-1]) return;     //Remove for unbounded
-                vis[i] = 1;                                              //Remove for unbounded
-                temp.push_back(num[i]);                                
-                dfs(num, vis,temp,ans);                                  //😍We check vis[i] but we dont pass index c in permutation, we call dfs for i=0 to nums.size()
-                temp.pop_back();            
-                vis[i] = 0;                                              ✅Unmark it so that element can be permuted with other things also//Remove for unbounded
-            }
-        }
-}
+        for c in range(0,len(nums)):
+            if(vis[c]==0):                                             #Remove for unbounded
+                if(c>0 and nums[c]== nums[c-1] and vis[c-1]): return   #Remove for unbounded
+                vis[c]=1                                               #Remove for unbounded
+                temp.append(nums[c])
+                self.dfs(nums,temp,ans,vis)                             #😍We check vis[i] but we dont pass index c in permutation, we call dfs for i=0 to nums.size()
+                vis[c]=0                                                #✅Unmark it so that element can be permuted with other things also//Remove for unbounded
+                temp.pop()
+
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        temp=[]
+        ans=[]
+        vis=[0]*len(nums)
+        self.dfs(nums,temp,ans,vis)
+        return ans
 
 
 // Rule : For bounded: If elements of input array may have same elements eg: 112, u cant remove them like , u have to necessarily use if(i>c && cand[i]==cand[i-1])continue; i,e 112 will be formed only 1 time in combination
