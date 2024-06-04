c38d837c273597218df347ce12759a6b90cb8a90
Amrit Rai <amritrai5757@gmail.com>
2024-06-05 00:54:41 +0530

    Update Square Submatrix.py

--- Recursion and Dp/Square Submatrix.py
+++ Recursion and Dp/Square Submatrix.py
@@ -4,9 +4,9 @@
 def countSquares(matrix):
     count = 0
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if i != 0 and j != 0 and matrix[i][j] == 1:
+    for i in range(0,len(matrix)):
+        for j in range(0,len(matrix[0])):
+            if(i != 0 and j != 0 and matrix[i][j]) == 1:
                 # Max length of square matrix from this index
                 # (in part of matrix we have traversed, i.e., upward left part considering the current index as the center of the graph paper)
                 # Max length of square matrix = max squares associated with the current index
