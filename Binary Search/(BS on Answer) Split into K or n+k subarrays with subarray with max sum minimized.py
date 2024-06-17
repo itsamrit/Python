3f154946cde2f25b17d3b5469559037f0a5d2ef0
Amrit Rai <amritrai5757@gmail.com>
2024-06-17 15:03:03 +0530

    Update (BS on Answer) Split into K or n+k subarrays with subarray with max sum minimized.py

--- Binary Search/(BS on Answer) Split into K or n+k subarrays with subarray with max sum minimized.py
+++ Binary Search/(BS on Answer) Split into K or n+k subarrays with subarray with max sum minimized.py
@@ -1,6 +1,10 @@
 TC : o(n*logn) n for checking is all subarrays can be divided into k parts with max cap mx & logn for binary search of max cap mx.
 // KOKO EATING BANANAS : array of n to be split in n+k size opposite of this
 # BINARY SEARCH ON ANS 
+
+# Binary Search on Answer is the algorithm in which we are finding our answer with the help of some particular conditions. 
+# We have given a search space in which we take an element [mid] and check its validity as our answer, 
+#     if it satisfies our given condition in the problem then we update the ans and reduce the search space accordingly.
 def valid(self,mid, h, piles):
         temph = 0
         for pile in piles:
