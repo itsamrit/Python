a061116b011f8ab88e36f9c637a5be55d1cef06b
Amrit Rai <amritrai5757@gmail.com>
2024-08-21 01:35:06 +0530

    Update Subset Combination Unbounded & 01(Subsequence).py

--- Recursion and Dp/Subset Combination Unbounded & 01(Subsequence).py
+++ Recursion and Dp/Subset Combination Unbounded & 01(Subsequence).py
@@ -1,19 +1,15 @@
-class Solution:
-    def dfs (self, nums,i,temp,ans):
-        if(i>=len(nums)):
-            ans.append(temp.copy())     # Shallow copy if u are appending temp instead of temp, u are appending reference of temp, so when temp become empty and becomes list of empty list
-            return
+def dfs (self, nums , c, temp, ans):
+    if c>=len(nums):
+        ans.add(tuple(temp))
+        return 
+    self.dfs(nums, c+1, temp, ans)
+    temp.append(nums[c])
+    self.dfs(nums, c+1, temp, ans)
+    temp.pop()
+    
 
-        temp.append(nums[i])            # ALWAYS APPEND FIRST 
-        self.dfs(nums,i+1,temp,ans)
-        temp.pop()
-        while(i<len(nums)-1  and nums[i] == nums[i+1]): i+=1   #AFTER WHILE call dfs without appended elemet
-        self.dfs(nums,i+1,temp,ans)
-        
-
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        ans=[]
-        temp=[]
-        nums.sort()
-        self.dfs(nums,0,temp,ans)
-        return ans
+def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+    ans =set()
+    nums = sorted(nums)
+    self.dfs(nums, 0, [], ans)
+    return list(ans)
