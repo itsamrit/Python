649027e905d6c0ccb28a51a506576ecdaea2ee5e
Amrit Rai <amritrai5757@gmail.com>
2024-08-22 22:35:39 +0530

    Update Subset Combination Unbounded & 01(Subsequence).py

--- Recursion and Dp/Subset Combination Unbounded & 01(Subsequence).py
+++ Recursion and Dp/Subset Combination Unbounded & 01(Subsequence).py
@@ -1,6 +1,6 @@
 def dfs (self, nums , c, temp, ans):
     if c>=len(nums):
-        ans.add(tuple(temp))
+        ans.add(tuple(temp)) # or ans.add(list(temp)) 
         return 
     self.dfs(nums, c+1, temp, ans)
     temp.append(nums[c])
@@ -10,6 +10,6 @@ def dfs (self, nums , c, temp, ans):
 
 def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
     ans =set()
-    nums = sorted(nums)
+    nums = sorted(nums)  # let arr=[1,2,1] then if not sorted [1,2] and [2,1] but they are same combination, TO avoid this we use the sorted with combination of set() 
     self.dfs(nums, 0, [], ans)
     return list(ans)
